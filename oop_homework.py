a8cfe844ea859aba86925fb1fd8d778a6181426a
Alexey Germanov <alger277@gmail.com>
2021-04-08 09:57:15 +0300

    Add emty strings between methods

--- oop_homework.py
+++ oop_homework.py
@@ -1,4 +1,5 @@
 class Student:
+
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -7,6 +8,7 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         self.avg_gr = 0
+
     def rate_lc(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
@@ -15,15 +17,18 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
     def avg(self):
         all=[]
         for lst in self.grades.values():
             all.extend(lst)
         self.avg_gr = sum(all) / len(all)
+
     def __lt__(self, other):
         if not isinstance(other, Student):
             print('Not a Student!')
         return self.avg_gr < other.avg_gr
+
     def __str__(self):
         return (
         f"\nИмя: {self.name}"
@@ -34,33 +39,40 @@ class Student:
         )
 
 class Mentor:
+
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         self.grades = {}
+
     def __str__(self):
         return f'Имя: {self.name} \nФамилия: {self.surname}'
 
 class Lecturer(Mentor):
+
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_attached = []
         self.grades = {}
         self.avg_gr = 0
+
     def avg(self):
         all=[]
         for lst in self.grades.values():
             all.extend(lst)
         self.avg_gr = sum(all) / len(all)
+
     def __lt__(self, other):
         if not isinstance(other, Lecturer):
             print('Not a Lecturer!')
         return self.avg_gr < other.avg_gr
+
     def __str__(self):
         return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекцию: {self.avg_gr}'
 
 class Reviewer(Mentor):
+
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_attached = []
@@ -74,5 +86,6 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+            
     def __str__(self):
         return f'Имя: {self.name} \nФамилия: {self.surname}'
